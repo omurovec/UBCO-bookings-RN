b8f40ad158bfa193db15f8efe977aedbe637b97c
Owen Murovec <owenmurovec@gmail.com>
2020-02-19 11:56:10 -0800

    Added type definitions

--- src/Functions.ts
+++ src/Functions.ts
@@ -3,7 +3,24 @@ import * as Keychain from "react-native-keychain";
 const URL =
 	"https://us-central1-ubco-bookings.cloudfunctions.net/";
 
-export default async (path, params) => {
+export default async (
+    path: string,
+    params?: {
+        year?: string;
+				month?: number;
+				day?: number;
+				startTime?: number;
+				endTime?: number;
+				area?: string;
+				roomID?: number;
+				title?: string;
+				description?: string;
+				roomName?: string;
+				areaName?: string;
+				email?: string;
+				phone?: string;
+    }
+) => {
 	const creds = await Keychain.getGenericPassword();
 	return fetch(URL + path, {
 		method: "POST",
@@ -22,6 +39,7 @@ export default async (path, params) => {
 			if (respJson.success) {
 				return respJson.data;
 			} else {
+          console.log(respJson);
 				throw "Data retrieval was unsuccessful";
 			}
 		});
